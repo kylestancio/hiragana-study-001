8f7b972463509f4f5afa3e198a2b0feaefaa3a57
Kyle Stephen Tancio <77538753+ksrtancio@users.noreply.github.com>
2023-03-27 01:52:49 +0900

    Initial Working Prototype: A - KA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Container } from 'react-bootstrap'
+import dataset from './dataset'
+// import PropTypes from 'prop-types';
 
 export default function App() {
+    
+  const [data, setData] = useState({})
+  const [showAnswer, setShowAnswer] = useState(false)
+
+  const getRandomCharacter = () => {
+    setShowAnswer(false)
+    setData(dataset[Math.floor(Math.random() * dataset.length)])
+  }
+
+  useEffect(() => {
+    document.title = "Hiragana Practice"
+    getRandomCharacter()
+
+    return () => {
+      setData({})
+    }
+  }, [])
+  
+
   return (
     <>
-      <h1 className='text-center'>Hello World</h1>
+      <Container>
+        <h1 className='text-center mt-5 '>Hiragana Training</h1>
+        <Card style={{width: '350px'}} className='mx-auto mt-4'>
+          <Card.Header> Write this in hiragana/katakana character </Card.Header>
+          <Card.Body>
+            <p className="display-2 text-center">{ showAnswer ? data.hiragana : data.furigana }</p>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant={`outline-${showAnswer ? 'danger' : 'primary'} me-3`} onClick={()=>{setShowAnswer(!showAnswer)}}>{showAnswer ? 'Hide' : 'Show'} Answer</Button>
+            <Button variant='outline-success' onClick={getRandomCharacter}>Next</Button>
+          </Card.Footer>
+        </Card>
+      </Container>
     </>
   )
 }
---
+++ src/dataset.js
@@ -0,0 +1,66 @@
+const dataset = [
+  {
+    "id":0,
+    "furigana": "A",
+    "hiragana": "あ",
+    "katakana": "ア"
+  },
+  {
+    "id":1,
+    "furigana": "I",
+    "hiragana": "い",
+    "katakana": "イ"
+  },
+  {
+    "id":2,
+    "furigana": "U",
+    "hiragana": "う",
+    "katakana": "ウ"
+  },
+  {
+    "id":3,
+    "furigana": "E",
+    "hiragana": "え",
+    "katakana": "エ"
+  },
+  {
+    "id":4,
+    "furigana": "O",
+    "hiragana": "お",
+    "katakana": "オ"
+  },
+
+
+  {
+    "id":5,
+    "furigana": "KA",
+    "hiragana": "か",
+    "katakana": "カ"
+  },
+  {
+    "id":6,
+    "furigana": "KI",
+    "hiragana": "き",
+    "katakana": "キ"
+  },
+  {
+    "id":7,
+    "furigana": "KU",
+    "hiragana": "く",
+    "katakana": "ク"
+  },
+  {
+    "id":8,
+    "furigana": "KE",
+    "hiragana": "け",
+    "katakana": "ケ"
+  },
+  {
+    "id":9,
+    "furigana": "KO",
+    "hiragana": "こ",
+    "katakana": "コ"
+  }
+]
+
+module.exports = dataset
