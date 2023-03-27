38f363f9fa5ca9c3c64f5e14e60de5ef390d0b26
Kyle Stephen Tancio <kylestancio@gmail.com>
2023-03-27 22:54:38 +0900

    Added to datalist: GA, SA, ZA, TA, DA

--- src/dataset.js
+++ src/dataset.js
@@ -1,30 +1,30 @@
 const dataset = [
   {
-    "id":0,
+    "id": 0,
     "furigana": "A",
     "hiragana": "あ",
     "katakana": "ア"
   },
   {
-    "id":1,
+    "id": 1,
     "furigana": "I",
     "hiragana": "い",
     "katakana": "イ"
   },
   {
-    "id":2,
+    "id": 2,
     "furigana": "U",
     "hiragana": "う",
     "katakana": "ウ"
   },
   {
-    "id":3,
+    "id": 3,
     "furigana": "E",
     "hiragana": "え",
     "katakana": "エ"
   },
   {
-    "id":4,
+    "id": 4,
     "furigana": "O",
     "hiragana": "お",
     "katakana": "オ"
@@ -32,34 +32,194 @@ const dataset = [
 
 
   {
-    "id":5,
+    "id": 5,
     "furigana": "KA",
     "hiragana": "か",
     "katakana": "カ"
   },
   {
-    "id":6,
+    "id": 6,
     "furigana": "KI",
     "hiragana": "き",
     "katakana": "キ"
   },
   {
-    "id":7,
+    "id": 7,
     "furigana": "KU",
     "hiragana": "く",
     "katakana": "ク"
   },
   {
-    "id":8,
+    "id": 8,
     "furigana": "KE",
     "hiragana": "け",
     "katakana": "ケ"
   },
   {
-    "id":9,
+    "id": 9,
     "furigana": "KO",
     "hiragana": "こ",
     "katakana": "コ"
+  },
+
+
+  {
+    "id": 10,
+    "furigana": "GA",
+    "hiragana": "が",
+    "katakana": "ガ"
+  },
+  {
+    "id": 11,
+    "furigana": "GI",
+    "hiragana": "ぎ",
+    "katakana": "ギ"
+  },
+  {
+    "id": 12,
+    "furigana": "GU",
+    "hiragana": "ぐ",
+    "katakana": "グ"
+  },
+  {
+    "id": 13,
+    "furigana": "GE",
+    "hiragana": "げ",
+    "katakana": "ゲ"
+  },
+  {
+    "id": 14,
+    "furigana": "GO",
+    "hiragana": "ご",
+    "katakana": "ゴ"
+  },
+
+
+  {
+    "id": 15,
+    "furigana": "SA",
+    "hiragana": "さ",
+    "katakana": "サ"
+  },
+  {
+    "id": 16,
+    "furigana": "SHI",
+    "hiragana": "し",
+    "katakana": "シ"
+  },
+  {
+    "id": 17,
+    "furigana": "SU",
+    "hiragana": "す",
+    "katakana": "ス"
+  },
+  {
+    "id": 18,
+    "furigana": "SE",
+    "hiragana": "せ",
+    "katakana": "セ"
+  },
+  {
+    "id": 19,
+    "furigana": "SO",
+    "hiragana": "そ",
+    "katakana": "ソ"
+  },
+
+
+  {
+    "id": 20,
+    "furigana": "ZA",
+    "hiragana": "ざ",
+    "katakana": "ザ"
+  },
+  {
+    "id": 21,
+    "furigana": "JI",
+    "hiragana": "じ",
+    "katakana": "ジ"
+  },
+  {
+    "id": 22,
+    "furigana": "ZU",
+    "hiragana": "ず",
+    "katakana": "ズ"
+  },
+  {
+    "id": 23,
+    "furigana": "ZE",
+    "hiragana": "ぜ",
+    "katakana": "ゼ"
+  },
+  {
+    "id": 24,
+    "furigana": "ZO",
+    "hiragana": "ぞ",
+    "katakana": "ゾ"
+  },
+
+
+  {
+    "id": 25,
+    "furigana": "TA",
+    "hiragana": "た",
+    "katakana": "タ"
+  },
+  {
+    "id": 26,
+    "furigana": "CHI",
+    "hiragana": "ち",
+    "katakana": "チ"
+  },
+  {
+    "id": 27,
+    "furigana": "TSU",
+    "hiragana": "つ",
+    "katakana": "ツ"
+  },
+  {
+    "id": 28,
+    "furigana": "TE",
+    "hiragana": "て",
+    "katakana": "テ"
+  },
+  {
+    "id": 29,
+    "furigana": "TO",
+    "hiragana": "と",
+    "katakana": "ト"
+  },
+
+
+  {
+    "id": 30,
+    "furigana": "DA",
+    "hiragana": "だ",
+    "katakana": "ダ"
+  },
+  {
+    "id": 31,
+    "furigana": "DJI",
+    "hiragana": "ぢ",
+    "katakana": "ヂ"
+  },
+  {
+    "id": 32,
+    "furigana": "DZU",
+    "hiragana": "づ",
+    "katakana": "ヅ"
+  },
+  {
+    "id": 33,
+    "furigana": "DE",
+    "hiragana": "て",
+    "katakana": "デ"
+  },
+  {
+    "id": 34,
+    "furigana": "DO",
+    "hiragana": "と",
+    "katakana": "ド"
   }
 ]
 
